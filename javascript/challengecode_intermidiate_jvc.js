af42147d9249b367b1eee735f44330eeecbbbc27
jamespipat <james.pipat08@gmail.com>
2020-02-28 17:05:26 +0700

    javascript changes

--- javascript/challengecode_intermidiate_jvc.js
+++ javascript/challengecode_intermidiate_jvc.js
@@ -111,3 +111,120 @@ const reverseArray = (arr) => {
     return sorted_year;
   };
 
+  const justCoolStuff = (arr1, arr2) => {
+    let inter_arr = [];
+    for (let word of arr1) {
+      if (arr2.includes(word)) {
+        inter_arr.push(word);
+      }
+    }
+    return inter_arr;
+  }
+  const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => secondArray.includes(item))
+
+  /*
+  // As a function declaration AND using named function w/ a loop:
+  function justCoolStuff(firstArray, secondArray) {
+        function isInSecondArray(item){
+              for(let i = 0; i<secondArray.length; i++){
+                    if (secondArray[i] === item){
+                          return true
+                    }
+              }
+              return false 
+        }
+        return firstArray.filter(isInSecondArray)
+  }
+  */
+  
+  const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+  
+  const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+  
+  console.log(justCoolStuff(myStuff, coolStuff))  
+
+
+
+  const isTheDinnerVegan = food => {
+    const isVegan = (food) => {
+      if(food.source === 'plant') {
+          return true;
+      }
+      return false; 
+    }
+    return food.every(isVegan);
+  }
+  
+  
+  
+  // const meal = [{name: 'arugula', source: 'plant'}, {name: 'tomatoes', source: 'plant'}, {name: 'lemon', source:'plant'}, {name: 'olive oil', source: 'plant'}];
+  
+  // console.log(meal[0].source);
+
+  // Feel free to comment out the code below to test your function
+  
+  const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
+  
+  console.log(isTheDinnerVegan(dinner))
+  // Should print false
+
+
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+const sortSpeciesByTeeth = arr => arr.sort((speciesObj1, speciesObj2) => speciesObj1.numTeeth > speciesObj2.numTeeth)
+
+/*
+// As a function declaration AND using a named function:
+function sortSpeciesByTeeth(arr) {
+      const compareTeeth = (speciesObj1, speciesObj2) => speciesObj1.numTeeth > speciesObj2.numTeeth
+      return arr.sort(compareTeeth)
+}
+*/
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+
+// Write your code here:
+const findMyKeys = (arr) => {
+  return arr.findIndex(element => element === "keys");
+}
+
+// Feel free to comment out the code below to test your function
+
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+console.log(findMyKeys(randomStuff))
+// Should print 4
+
+
+const dogFactory = (name, breed, weight) => {
+  return {
+    _name: name,
+    _breed: breed,
+    _weight: weight,
+    get name() {
+      return this._name;
+    },
+    set name(name) {
+      this._name = name;
+    },
+    get breed() {
+      return this._breed; 
+    },
+    set breed(breed) {
+      this._breed = breed;
+    },
+    get weight() {
+      return this._weight;
+    },
+    set weight(weight) {
+      this._weight = weight;
+    },
+    bark() {
+      return 'ruff! ruff!';
+    },
+    eatTooManyTreats() {
+      this._weight += 1;
+    }
+  }
+}
---
+++ javascript/lodash/_.js
@@ -0,0 +1,4 @@
+// Do not write or modify code below this line.
+module.exports = _;
+
+cosnt _ = {};
