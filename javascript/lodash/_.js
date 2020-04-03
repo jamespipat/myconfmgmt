35926291e784911a75c00b7643c0e5cb1579a977
jamespipat <james.pipat08@gmail.com>
2020-04-03 17:17:43 +0700

    ansible changes and javascript

--- javascript/lodash/_.js
+++ javascript/lodash/_.js
@@ -1,4 +1,27 @@
 // Do not write or modify code below this line.
-module.exports = _;
+module.exports = "_";
 
-cosnt _ = {};
+const _ = {
+    clamp(num, lower, upper) {
+        let lowerClampedValue = Math.max(num, lower);
+        let clampedValue = Math.min(lowerClampedValue, upper);
+        return clampedValue;
+    },
+    inRange(num, start, end) {
+        if (num == undefined) {
+            end = start;
+            start = 0;
+        }
+        if (start > end) {
+            tempend = end;
+            start = end;
+            end = tempend;
+        }
+        const isInRange = (start <= num && num < end);
+        return isInRange;
+
+    },
+    words(word) {
+
+    }
+};
